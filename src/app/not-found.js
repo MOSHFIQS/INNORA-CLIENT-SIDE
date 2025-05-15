511eec2eb35c7bc01d5e894c8635699d0e88a58c
nibirrahman123 <nibir3211@gmail.com>
2025-05-16 00:00:58 +0600

    firebase setup done

---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const notFound = () => {
+  return (
+    <div className='text-center h-screen flex items-center justify-center'>
+      Sorry this is UnAvailable
+    </div>
+  )
+}
+
+export default notFound
